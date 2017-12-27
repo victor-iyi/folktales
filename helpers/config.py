a31d7bc380444d80dc1fff1a2e0d04e438a9b3f2
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-12-27 22:10:44 +0100

    Added .gitignore

---
+++ helpers/config.py
@@ -0,0 +1,9 @@
+"""
+  @author Victor I. Afolabi
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  
+  Created on 27 December, 2017 @ 12:40 AM.
+  
+  Copyright © 2017. Victor. All rights reserved.
+"""
