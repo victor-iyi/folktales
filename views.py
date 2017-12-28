cceb09c6b378c446ceeffaad9ee48760b9f4512f
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-12-28 17:44:34 +0100

    Views

--- views.py
+++ views.py
@@ -21,3 +21,23 @@ def index():
     :return template
     """
     return render_template('index.html')
+
+
+@app.route('/left-sidebar/')
+def left_sidebar():
+    return render_template('left-sidebar.html')
+
+
+@app.route('/right-sidebar/')
+def right_sidebar():
+    return render_template('right-sidebar.html')
+
+
+@app.route('/no-sidebar/')
+def no_sidebar():
+    return render_template('no-sidebar.html')
+
+
+@app.route('/elements')
+def elements():
+    return render_template('elements.html')
