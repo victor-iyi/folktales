20441ad29df410d9588b43b54eb9a1d797d679c7
Victor I. Afolabi <javafolabi@gmail.com>
2018-01-25 20:49:59 +0100

    Update views/__init__.py

--- views/__init__.py
+++ views/__init__.py
@@ -1,8 +1,10 @@
 """
   @author Victor I. Afolabi
   A.I. Engineer & Software developer
+
   javafolabi@gmail.com
   Created on 25 January, 2018 @ 8:25 PM.
+
   Copyright © 2018. Victor. All rights reserved.
 """
 from flask import Flask
