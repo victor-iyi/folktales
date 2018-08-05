081b9c1fde5ac69e3d1baf2e5ad7c3f5f1b93267
Victor I. Afolabi <javafolabi@gmail.com>
2018-08-05 18:45:47 +0100

    Story Analysis

--- model/analyzer.py
+++ model/analyzer.py
@@ -1,41 +1,47 @@
-import os.path
 import re
 
 import nltk
 
-from model.utils import *
-
-
-def main():
-    """Start program execution."""
-    pass
-
 
 def characters(tagged):
+    """Retrieve all the main characters in the story."""
     entities = nltk.ne_chunk(tagged, binary=True)
     matches = re.findall(r'\(NE\s(\w*)/NNP', str(entities))
-    return set(matches)
+    return list(sorted(set(matches)))
 
 
 def songs(story: str):
+    """Get the songs in this story."""
     return []
 
 
 def lessons(story: str):
+    """Get all moral lessons in the story."""
     return []
 
 
 def locations(tagged):
+    """Get locations in the story."""
     entities = nltk.ne_chunk(tagged, binary=False)
     matches = re.findall(r'\(GPE\s(\w*?)\/NNP\)', str(entities))
-    return set(matches)
+    return list(sorted(set(matches)))
 
 
 def proverbs(tagged):
     return []
 
 
-def analyze(story):
+def analyze(story: str):
+    """Analyze a given story.
+
+    Args:
+        story (str): Story to be analyzed.
+
+    Returns:
+        dict - Containing analysis of characters, locations,
+            songs, provers and moral lessons.
+    """
+
     token = nltk.word_tokenize(story)
     tagged = nltk.pos_tag(token)
 
@@ -47,7 +53,3 @@ def analyze(story):
     }
 
     return results
-
-
-if __name__ == '__main__':
-    main()
